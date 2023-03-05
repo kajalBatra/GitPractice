f7cedb7d9fb2a7a369f9b1301c08116643eb597c
Kajal Batra <>
2023-03-05 12:53:29 +0530

    Added push notification

--- GitPractice/GitPractice/AppDelegate.swift
+++ GitPractice/GitPractice/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { access, error in
+            
+        }
+        
         return true
     }
 
